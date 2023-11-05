5ff0632f6545e600bba9048a3457aa8d1d6d6098
Wilhelm Payne <wbpayne@wilhelmpayne.com>
2023-11-04 22:10:46 -0400

    Add project files.

---
+++ CSConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CSConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            Console.WriteLine($"Hello, World! {a}");
+            Console.WriteLine("This is a demonstration program!");
+            for(int x = 1; x<=15; x++)
+            {
+                Console.WriteLine($"Hi {x}");
+            }
+        }
+    }
+}
